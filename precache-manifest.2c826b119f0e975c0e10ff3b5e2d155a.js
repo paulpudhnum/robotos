9c658bd51128795a73eebf32d5fba314d685a8da
Paul Pudhnum <smarterbusiness@gmail.com>
2020-10-19 15:57:09 -0700

    Updates

---
+++ precache-manifest.2c826b119f0e975c0e10ff3b5e2d155a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21ace52d3acb1b4508a1f0827d90594d",
+    "url": "/robotos/index.html"
+  },
+  {
+    "revision": "f4ce1e75a97cecf1d280",
+    "url": "/robotos/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4a310e4b540b5ff85a6d",
+    "url": "/robotos/static/css/main.fb3cba5d.chunk.css"
+  },
+  {
+    "revision": "f4ce1e75a97cecf1d280",
+    "url": "/robotos/static/js/2.77e089d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robotos/static/js/2.77e089d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a310e4b540b5ff85a6d",
+    "url": "/robotos/static/js/main.1fa9f3cf.chunk.js"
+  },
+  {
+    "revision": "4ca93128bce6e6a8b6dc",
+    "url": "/robotos/static/js/runtime-main.1388e79b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotos/static/media/SEGA.12a3db8b.woff"
+  }
+]);
